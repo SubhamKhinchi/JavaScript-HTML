f7538b1301436327ecef384fbb1fa0854f82d76b
TheGuyNextToYou <subhamkhinchi018@gmail.com>
2020-02-09 04:08:58 +0530

    Add files via upload

---
+++ BasicCodes/if_else_if.js
@@ -0,0 +1,13 @@
+var x =10;
+if(x==30){
+    document.write("x is equal to 30");
+}
+else if (x==20){
+    document.write("x is equal to 20");
+}
+else if(x==10){
+    document.write("x is equal to 10");
+}
+else{
+    document.write("x is not equal to 30,20 and 10");
+}
