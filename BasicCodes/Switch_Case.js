eeff827b1ca7bb3ccdcaeb39c818e4dd99f3bb78
TheGuyNextToYou <subhamkhinchi018@gmail.com>
2020-02-08 18:24:16 +0530

    Add files via upload

---
+++ BasicCodes/Switch_Case.js
@@ -0,0 +1,20 @@
+var Grade = 'B';
+var result;
+switch(Grade){
+    case 'A' :
+        result = "A Grade";
+        break;
+    case 'B' :
+        result = "B Grade";
+        break;
+    case 'C' :
+        result = "C Grade";
+        break;
+    case 'D' :
+        result = "D Grade";
+        break;
+    case 'F' :
+        result = "F Grade";
+        break; 
+}
+document.write(result);
